e531731359a59e3334d0dd20ad2d5d5b8a2b81e4
Kevin Yang <chgc_kevin@hotmail.com>
2019-07-09 14:52:28 +0800

    extract title from search result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WikiSearchResult } from './wiki-search.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,10 @@ export class AppComponent {
     this.http
       .get<WikiSearchResult>(
         'https://en.wikipedia.org//w/api.php?action=query&format=json&origin=*&list=search&srsearch=angular'
+      )
+      .pipe(
+        map(data => data.query.search),
+        map(search => search.map(x => x.title))
       )
       .subscribe(data => console.log(data));
   }
